0224327aace7588d517dba1481d9ba6983330c4e
solgrey <sol.medina@duocuc.cl>
2025-10-21 20:33:13 -0300

    ConfigViewModel

--- app/src/main/java/com/duocuc/serena/navigation/AppNav.kt
+++ app/src/main/java/com/duocuc/serena/navigation/AppNav.kt
@@ -6,6 +6,23 @@ import androidx.navigation.NavHostController
 @Composable
 fun AppNavHost (nav: NavHostController){
 
+    val configVM: ConfigViewModel = viewModel()
+    val perfilVM: PerfilViewModel = viewModel()
+    val productoVM: ProductoViewModel = viewModel()
 
+    NavHost(navController = nav, startDestination = Route.Home.path, route = Route.Root.path) {
+        composable(Route.Home.path) {
+            HomeScreen(nav, configVM)
+        }
+        composable(Route.Perfil.path) {
+            PerfilScreen(nav, perfilVM)
+        }
+        composable(Route.Registrar.path) {
+            RegistroScreen(nav, productoVM)
+        }
+        composable(Route.Resumen.path) {
+            ResumenScreen(nav, productoVM)
+        }
+    }
 
 }
