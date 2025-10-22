394e9beb4d49eee74ba4b8e39cf296de971aee5f
solgrey <sol.medina@duocuc.cl>
2025-10-21 21:33:55 -0300

    UserDAO

--- app/src/main/java/com/duocuc/serena/DAO/UserDAO.kt
+++ app/src/main/java/com/duocuc/serena/DAO/UserDAO.kt
@@ -1,8 +1,11 @@
 package com.duocuc.serena.DAO
 
 import androidx.annotation.IntegerRes
+import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.Query
 import com.duocuc.serena.data.UserData
+import com.duocuc.serena.model.UserUiState
 
 interface UserDAO {
 
@@ -14,6 +17,19 @@ interface UserDAO {
     @Query("SELECT * FROM usuarios")
     fun getAll(): List<UserData>
 
+    //Buscar por ID
+    @Query("SELECT * FROM usuarios WHERE id IN (:userIds)")
+    fun loadAllByIds(userIds: IntArray): List<UserData>
+
+    @Query("SELECT * FROM usuarios WHERE nombreUsuario LIKE :nombreUsuario AND " +
+            "nombreUsuario LIKE :last LIMIT 1")
+    fun findByName(first: String, last: String): UserUiState
+
+    @Insert
+    fun insertAll(vararg users: UserData)
+
+    @Delete
+    fun delete(user: UserData)
 
     //Agregar Consultas Personalizadas
 
