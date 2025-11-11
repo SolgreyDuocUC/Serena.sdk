50145fc6fd2f80baecff6226b03afdf7a87e91cf
Solgrey <sol.medina@duocuc.cl>
2025-11-11 03:33:18 -0300

    Registro emocional v.2

--- app/src/main/java/com/duocuc/serena/DAO/RegistroEmocionalDao.kt
+++ app/src/main/java/com/duocuc/serena/DAO/RegistroEmocionalDao.kt
@@ -1,10 +1,8 @@
 package com.duocuc.serena.dao
 
-
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.*
 import com.duocuc.serena.data.EmotionalRegisterData
+import java.time.LocalDate
 
 @Dao
 interface RegistroEmocionalDao {
@@ -12,9 +10,17 @@ interface RegistroEmocionalDao {
     @Insert
     suspend fun insertEmotion(register: EmotionalRegisterData)
 
+    @Update
+    suspend fun updateEmotion(register: EmotionalRegisterData)
+
+    @Delete
+    suspend fun deleteEmotion(register: EmotionalRegisterData)
+
     @Query("SELECT * FROM RegistroEmocionalUsuario")
     suspend fun getAllRegisters(): List<EmotionalRegisterData>
 
-    @Query("SELECT * FROM RegistroEmocionalUsuario WHERE idEmocion = :emotionId")
-    suspend fun getRegistersByEmotion(emotionId: Int): List<EmotionalRegisterData>
+    @Query("SELECT * FROM RegistroEmocionalUsuario WHERE id = :id")
+    suspend fun getRegistersById(id: Int): List<EmotionalRegisterData>
 }
+
+
